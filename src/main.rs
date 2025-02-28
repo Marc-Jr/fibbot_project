0bf291503f933aefbd4bd1b9ba1323ba53c4fc65
Marc-Jr <atebemarcjr@icloud.com>
2025-02-28 17:29:35 +0100

    modified src main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
 fn main() {
     println!("Hello, world!");
+
+    let enable_fib = env::var("INPUT_ENABLE_FIB").unwrap_or("true".to_string());
+    let max_threshold: usize = env::var("INPUT_MAX_THRESHOLD")
+        .unwrap_or("1000".to_string())
+        .parse()
+        .unwrap_or(1000);
+
+    println!("Enable Fibonacci: {}", enable_fib);
+    println!("Max Threshold: {}", max_threshold);
 }
+
+
+
+
---
+++ target/debug/fibbot_project.d
@@ -0,0 +1,1 @@
+/Users/gisstudent/Projects/rust-projects/fibbot_project/target/debug/fibbot_project: /Users/gisstudent/Projects/rust-projects/fibbot_project/src/main.rs
