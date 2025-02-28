764d9150555749384f11d9f09885d16da15e543f
Marc-Jr <atebemarcjr@icloud.com>
2025-02-28 17:22:36 +0100

    Add Hello world Action

---
+++ target/debug/deps/fibbot_project-02273965d750388d.d
@@ -0,0 +1,5 @@
+/Users/gisstudent/Projects/rust-projects/fibbot_project/target/debug/deps/libfibbot_project-02273965d750388d.rmeta: src/main.rs
+
+/Users/gisstudent/Projects/rust-projects/fibbot_project/target/debug/deps/fibbot_project-02273965d750388d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/fibbot_project-2f36a2138c34a9d8.d
@@ -0,0 +1,5 @@
+/Users/gisstudent/Projects/rust-projects/fibbot_project/target/debug/deps/libfibbot_project-2f36a2138c34a9d8.rmeta: src/main.rs
+
+/Users/gisstudent/Projects/rust-projects/fibbot_project/target/debug/deps/fibbot_project-2f36a2138c34a9d8.d: src/main.rs
+
+src/main.rs:
